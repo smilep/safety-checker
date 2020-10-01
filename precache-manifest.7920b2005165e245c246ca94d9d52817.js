994f7cc3232dc174625a7c95c3249d32056df52e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-01 10:32:56 +0000

    Deploy smilep/safety-checker to github.com/smilep/safety-checker.git:gh-pages

---
+++ precache-manifest.7920b2005165e245c246ca94d9d52817.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55af3200881362edb16974f362f15540",
+    "url": "/safety-checker/index.html"
+  },
+  {
+    "revision": "5b01df66fe0ce99761de",
+    "url": "/safety-checker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b3eb1407206841f2d43f",
+    "url": "/safety-checker/static/css/main.3cd36fed.chunk.css"
+  },
+  {
+    "revision": "5b01df66fe0ce99761de",
+    "url": "/safety-checker/static/js/2.135d027f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/safety-checker/static/js/2.135d027f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3eb1407206841f2d43f",
+    "url": "/safety-checker/static/js/main.afda2a61.chunk.js"
+  },
+  {
+    "revision": "340cd807ca12d7b52c37",
+    "url": "/safety-checker/static/js/runtime-main.5895088e.js"
+  },
+  {
+    "revision": "c6609bccc44eb321f34c583496faccd3",
+    "url": "/safety-checker/static/media/maybe.c6609bcc.gif"
+  },
+  {
+    "revision": "edeb6af23dcdd0f1e53fdb0b9296c5de",
+    "url": "/safety-checker/static/media/no.edeb6af2.gif"
+  }
+]);
